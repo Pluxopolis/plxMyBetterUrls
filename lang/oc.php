71ef142497a07384777ff43f1bddb315ac654ef6
Quent-in <pages_quentin@hotmail.com>
2016-04-20 19:48:57 +0200

    Occitan version

---
+++ lang/oc.php
@@ -0,0 +1,12 @@
+<?php
+$LANG = array(
+# config.php
+'L_BAD_URL_EXTENSION'	=> 'Marrit format de l\'extension de l\'url',
+'L_URLS_EXTENSION'		=> 'Extension de las urls',
+'L_URLS_FORMAT'			=> 'Format de las urls :<br />Daissatz los camps voids per utilizar lo format per defaut de MyBetterUrls',
+'L_SAVE'				=> 'Enregistrar',
+'L_ARTICLE'				=> 'Article',
+'L_CATEGORY'			=> 'Categoria',
+'L_STATIC'				=> 'Pagina estatica'
+);
+?>
